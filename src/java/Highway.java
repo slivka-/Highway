f6f10065b4afcbfcf7abdfbcb0bdd2f420123987
slivka- <slivka104@gmail.com>
2017-11-03 14:15:58 +0100

    Added jdbc driver loading

--- src/java/Highway.java
+++ src/java/Highway.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Enumeration;
@@ -29,34 +30,67 @@ public class Highway extends HttpServlet
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         response.setContentType("text/html;charset=UTF-8");
-        
         String dbString = "";
+        //Check url parameters for jdbc connection string
         Enumeration<String> paramNames = request.getParameterNames();
         while(paramNames.hasMoreElements())
         {
             String paramValue = request.getParameter(paramNames.nextElement());
             if(paramValue.startsWith("jdbc"))
             {
+                //if parameter line starts with jdbc, save it as connection string and break
                 dbString = paramValue;
                 break;
             }
         }
+        //start if jdbc connection string was found
         if(!dbString.equalsIgnoreCase(""))
         {
-            
+            //split connection string to get database type
+            String dbType = dbString.split("\\:")[1].toLowerCase();
             String output = "";
             try
             {
-                Class.forName("org.sqlite.JDBC");
+                //load appropriate driver based on connection string
+                switch(dbType)
+                {
+                    case "sqlite":
+                        Class.forName("org.sqlite.JDBC");                        
+                        break;
+                    case "oracle":
+                        Class.forName("oracle.jdbc.OracleDriver");
+                        break;
+                    case "mysql":
+                        Class.forName("com.mysql.jdbc.Driver");
+                        break;
+                    case "sqlserver":
+                        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                        break;
+                    case "postgresql":
+                        Class.forName("org.postgresql.Driver");
+                    default:
+                        throw new ClassNotFoundException();
+                }
+
                 try(Connection conn = DriverManager.getConnection(dbString))
                 {
-                    
+                    output = "OK";
+                }
+                
+                //Unload all used drivers
+                Enumeration<Driver> drivers = DriverManager.getDrivers();
+                while(drivers.hasMoreElements())
+                {
+                    Driver d = drivers.nextElement();
+                    DriverManager.deregisterDriver(d);
                 }
             
             }
             catch(SQLException | ClassNotFoundException ex)
             {
-                output = ex.toString();
+                output = ex.toString() + "<br/>Call Stack:<br/>";
+                for(StackTraceElement el : ex.getStackTrace())
+                    output += el.toString()+"<br/>";
             }
             finally
             {
